c8c30d715bd0f6604ab9861b4d882755db183b96
Cherry <charissa@barebarrel.com>
2026-02-05 15:10:25 +0100

    Update Rymora product master data

--- models/staging/google_sheets/stg_google_sheets__rymora_product_md.sql
+++ models/staging/google_sheets/stg_google_sheets__rymora_product_md.sql
@@ -8,47 +8,49 @@ rymora_product_md as (
 
 ),
 
-remove_product_group_rows as (
-
-    select *
-    from rymora_product_md
-    where
-        `ASIN` is not null
-
-),
-
 rename_fields as (
 
     select
-        `Product_Code` as product_code,
-        `Product_Group` as product_group,
-        `EAN` as ean,
-        `ASIN` as asin,
-        `Colour` as color,
-        `Size` as product_size,
-        `Current_SKU__NEW_` as current_sku,
-        `Previous_SKU__OLD_` as previous_sku,
-        `Previous_SKU__OLD_2_` as old_sku,
-        `MOQ` as moq,
-        `Lead_time__days_` as lead_time_days,
-        `Pcs__CTN` as pcs_per_carton,
-        _2020_rates as _2020_rates_usd,
-        `L__cm_` as length_cm,
-        `W__cm_` as width_cm,
-        `H__cm_` as height_cm,
-        `CBM` as cbm,
-        `N_W__kg_` as n_weight_kg,
-        `G_W__kg_` as g_weight_kg,
-        `L__in_` as length_inch,
-        `W__in_` as width_inch,
-        `H__in_` as height_inch,
-        `CFT` as cft,
-        `N_W__lb_` as n_weight_lb,
-        `G_W__lb_` as g_weight_lb,
-        per_unit_g,
-        per_unit_oz
+        string_field_0 as product_code,
+        string_field_1 as product_group,
+        string_field_2 as ean,
+        string_field_3 as asin,
+        string_field_4 as color,
+        string_field_5 as product_size,
+        string_field_6 as current_sku,
+        string_field_7 as previous_sku,
+        string_field_8 as old_sku,
+        string_field_9 as old_sku_2,
+        string_field_10 as moq,
+        string_field_11 as lead_time_days,
+        string_field_12 as pcs_per_carton,
+        string_field_13 as _2020_rates_usd,
+        string_field_14 as length_cm,
+        string_field_15 as width_cm,
+        string_field_16 as height_cm,
+        string_field_17 as cbm,
+        string_field_18 as n_weight_kg,
+        string_field_19 as g_weight_kg,
+        string_field_20 as length_inch,
+        string_field_21 as width_inch,
+        string_field_22 as height_inch,
+        string_field_23 as cft,
+        string_field_24 as n_weight_lb,
+        string_field_25 as g_weight_lb,
+        string_field_26 as per_unit_g,
+        string_field_27 as per_unit_oz
 
-    from remove_product_group_rows
+    from rymora_product_md
+
+),
+
+remove_product_group_rows as (
+
+    select *
+    from rename_fields
+    where
+        asin is not null
+        and asin is distinct from "ASIN"
 
 ),
 
@@ -64,13 +66,14 @@ remove_dollar_sign as (
         current_sku,
         previous_sku,
         old_sku,
+        old_sku_2,
         moq,
         lead_time_days,
         pcs_per_carton,
         case
             when _2020_rates_usd is null
                 then _2020_rates_usd
-            else REGEXP_EXTRACT(_2020_rates_usd, r'\d+\.\d*')
+            else REGEXP_EXTRACT(_2020_rates_usd, r"\d+\.\d*")
         end as _2020_rates_usd,
         length_cm,
         width_cm,
@@ -87,7 +90,7 @@ remove_dollar_sign as (
         per_unit_g,
         per_unit_oz
 
-    from rename_fields
+    from remove_product_group_rows
 
 ),
 
@@ -104,6 +107,7 @@ cast_data_types as (
         current_sku,
         previous_sku,
         old_sku,
+        old_sku_2,
 
         -- numerics
         CAST(moq as integer) as moq,
