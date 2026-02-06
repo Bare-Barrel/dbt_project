02ed82000136e982f57b6c07b15f8f33b35788a8
Cherry <charissa@barebarrel.com>
2026-02-06 23:18:37 +0100

    Add rymora warehouse stock levels data

---
+++ models/marts/supply_chain/obt_rymora_warehouse_stock_levels.sql
@@ -0,0 +1,34 @@
+-- obt_rymora_warehouse_stock_levels.sql
+
+with
+
+rymora_warehouse_stock_levels as (
+
+    select * from {{ ref('stg_mintsoft_api__rymora_warehouse_stock_levels') }}
+
+),
+
+reorder_fields as (
+
+    select
+        client_id,
+        account_name,
+        warehouse_id,
+        warehouse_name,
+        product_id,
+        sku,
+        stock_level,
+        total_stock_level,
+        preorderable,
+        bundle,
+        low_stock_level,
+        last_updated,
+        breakdown,
+        recorded_at,
+        recorded_date
+
+    from rymora_warehouse_stock_levels
+
+)
+
+select * from reorder_fields
