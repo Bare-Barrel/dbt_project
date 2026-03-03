6310ae33d87c8f5cd111d9041cdacc68d6511ed9
Cherry <charissa@barebarrel.com>
2026-03-03 04:57:38 +0100

    Add dim_parent_portfolio_code

---
+++ models/marts/master_data/dim_parent_portfolio_code.sql
@@ -0,0 +1,54 @@
+-- dim_parent_portfolio_code.sql
+
+with
+
+dim_products as (
+
+    select * from {{ ref('dim_products') }}
+
+),
+
+select_fields as (
+
+    select
+        portfolio_code,
+        parent_code,
+        product_type,
+        tenant_id
+
+    from dim_products
+
+),
+
+get_unique_rows as (
+
+    select distinct * from select_fields
+
+),
+
+remove_duplicates as (
+
+    select
+        portfolio_code,
+        parent_code,
+        product_type,
+        tenant_id
+
+    from (
+        select
+            portfolio_code,
+            parent_code,
+            product_type,
+            tenant_id,
+            row_number() over (
+                partition by portfolio_code
+                order by product_type asc
+            ) as row_num
+        from get_unique_rows
+    ) as t
+
+    where row_num = 1
+
+)
+
+select * from remove_duplicates
