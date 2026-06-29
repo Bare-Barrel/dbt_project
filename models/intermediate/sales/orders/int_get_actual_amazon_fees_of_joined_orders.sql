4d1837d7bddd167c184cb2c38771eca8a27d9af2
Cherry <charissa@barebarrel.com>
2026-06-30 01:08:54 +0200

    Convert order datetime to local marketplace timezone

--- models/intermediate/sales/orders/int_get_actual_amazon_fees_of_joined_orders.sql
+++ models/intermediate/sales/orders/int_get_actual_amazon_fees_of_joined_orders.sql
@@ -27,8 +27,8 @@ joined_orders_usd as (
 
     {% if is_incremental() %}
         where purchase_date_local >= (
-            select date_sub(max(this.purchase_date_local), interval 7 day)
-            from {{ this }} as this
+            select date_sub(max(existing.purchase_date_local), interval 7 day)
+            from {{ this }} as existing
         )
     {% else %}
         where purchase_date_local >= date '{{ var("initial_load_start", "1970-01-01") }}'
